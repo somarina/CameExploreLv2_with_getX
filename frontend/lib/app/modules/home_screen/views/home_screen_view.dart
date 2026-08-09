faabf748354a1bbc78623bf80c40f936d231c62e
somarina <somarinak@gmail.com>
2026-08-10 06:54:35 +0700

    update error

--- frontend/lib/app/modules/home_screen/views/home_screen_view.dart
+++ frontend/lib/app/modules/home_screen/views/home_screen_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element, unused_local_variable
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
